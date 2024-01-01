8b7045e8a4c8a7ba4da5f281efb7c293ba364447
Isaac <git@eggsampler.com>
2024-01-02 00:21:33 +1000

    Second pass

--- outputs/pdf2png.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p png
-rm -rf png/*
-pdftoppm PCIeDMA.pdf png/PCIeDMA.pdf -png -r 600
-pdftoppm PCIeDMA-Assembly.pdf png/PCIeDMA-Assembly.pdf -png -r 600
-pngquant --ext .png -f --skip-if-larger --speed 1 --strip png/*.png
